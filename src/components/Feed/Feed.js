39c71aa500fdb95c29406a2f8af86ee2fc303ee8
omer penso <omer.penso@post.runi.ac.il>
2024-10-14 22:20:53 +0300

    Implement pagination for better post navigation

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -8,6 +8,8 @@ import { useState } from "react";
 const Feed = () => {
     const { data: posts, loading, error } = useFetch("https://jsonplaceholder.typicode.com/posts");
     const [selectedOptions, setSelectedOptions] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const postsPerPage = 5;
 
     const uniqueAuthors = [...new Set(posts.map(post => post.userId))];
     const options = uniqueAuthors.map(authorID => ({
@@ -19,8 +21,13 @@ const Feed = () => {
     ? posts.filter(post => selectedOptions.some(option => option.value === post.userId))
     : posts;
 
+    const indexOfStartPost = (currentPage - 1) * postsPerPage;;
+    const indexOfEndPost = currentPage * postsPerPage;
+    const currentPosts = filteredPosts.slice(indexOfStartPost, indexOfEndPost);
+
     const handleChange = (selectedOptions) => {
         setSelectedOptions(selectedOptions || []); 
+        setCurrentPage(1);
     };
 
     return (
@@ -36,7 +43,24 @@ const Feed = () => {
 
             {loading && <p id="loading-text">Loading posts...</p>}
             {error && <p>{error}</p>}
-            {!loading && !error && <PostList posts={filteredPosts} />}
+            {!loading && !error && <PostList posts={currentPosts} />}
+
+            <button 
+                onClick={() => setCurrentPage(prev => Math.max(1, prev - 1))}
+                disabled={currentPage === 1}
+                className="previous-button"
+                > Previous Page 
+            </button>
+
+            <span>Page {currentPage}</span>
+
+            <button 
+                onClick={() => setCurrentPage((prev) => currentPage + 1)}
+                disabled={currentPosts.length < postsPerPage}
+                className="next-button"
+                > Next Page
+            </button>
+                
         </div>
     );
 };
